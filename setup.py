f0d3f9d8528b7051577d0ca54b367bc5aea14b0c
Sam Wilson <tecywiz121@hotmail.com>
2016-11-14 14:31:13 -0500

    Fix minor packaging details

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='Launch a process in an identifiable session',
     long_description=long_description,
 
     # The project's main homepage.
-    url='',
+    url='https://github.com/tecywiz121/imdo/',
 
     # Author details
     author='Sam Wilson',
